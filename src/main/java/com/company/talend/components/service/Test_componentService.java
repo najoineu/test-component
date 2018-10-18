7eb140a78240b0b772f9dd2e0c3f9fa5676308fc
njozefzoon <njozefzoon@xsed.nl>
2018-10-18 11:13:15 +0200

    Talend component Kit project

---
+++ src/main/java/com/company/talend/components/service/Test_componentService.java
@@ -0,0 +1,9 @@
+package com.company.talend.components.service;
+
+import org.talend.sdk.component.api.service.Service;
+
+@Service
+public class Test_componentService {
+
+
+}
---
+++ src/main/java/com/company/talend/components/source/TestComponentMapperConfiguration.java
@@ -0,0 +1,28 @@
+package com.company.talend.components.source;
+
+import java.io.Serializable;
+
+import org.talend.sdk.component.api.configuration.Option;
+import org.talend.sdk.component.api.configuration.ui.layout.GridLayout;
+import org.talend.sdk.component.api.meta.Documentation;
+
+@GridLayout({
+    // the generated layout put one configuration entry per line,
+    // customize it as much as needed
+    @GridLayout.Row({ "name" })
+})
+@Documentation("TODO fill the documentation for this configuration")
+public class TestComponentMapperConfiguration implements Serializable {
+    @Option
+    @Documentation("TODO fill the documentation for this parameter")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public TestComponentMapperConfiguration setName(String name) {
+        this.name = name;
+        return this;
+    }
+}
